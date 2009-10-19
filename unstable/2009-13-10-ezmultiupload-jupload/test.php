49b35e96e4cc469ce27c7cb5a35409fbb843975f
chen <info@example.com>
2009-10-19 07:45:00 +0000

    remove test.php and uploadresult.txt
    git-svn-id: http://svn.projects.ez.no/ezmultiupload@70 d44cdf02-dba6-4dc1-9069-7147f5ab4e2f

--- unstable/2009-13-10-ezmultiupload-jupload/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "ddd";
-?>
-<form enctype="multipart/form-data" method="post" action="test.php">
-<input type="file" name="f"></input>
-<input type="submit" value="submit"></input>
-</form>
